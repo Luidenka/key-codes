93466ab5d1c69f1c0424ede195507eb42c8837b1
Luidenka <jarvislui1128@gmail.com>
2022-06-15 10:00:35 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 document.body.addEventListener('keydown', (e) => {
-    document.getElementById('key').innerText = e.key;
-    document.getElementById('key-table').innerText = e.key;
+    if (e.key = "") {
+        document.getElementById('key').innerText = " ";
+        document.getElementById('key-table').innerText = " ";
+    } else {
+        document.getElementById('key').innerText = e.key;
+        document.getElementById('key-table').innerText = e.key;
+    }
     document.getElementById('keyCode').innerText = e.keyCode;
     document.getElementById('code').innerText = e.code;
     document.getElementById('location').innerText = e.location;
