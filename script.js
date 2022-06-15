e6fb71f1f82dd76f4aaab6765acee78e26055448
Luidenka <jarvislui1128@gmail.com>
2022-06-15 09:51:59 +0800

    Key code display

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 document.body.addEventListener('keypress', (e) => {
-    console.log(e);
+    document.getElementById('key').innerText = e.key;
+    document.getElementById('keyCode').innerText = e.keyCode;
+    document.getElementById('code').innerText = e.code;
+    document.getElementById('location').innerText = e.location;
 })
