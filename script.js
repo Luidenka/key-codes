63bccb93e707c5cc45415e9565e09d78441ed117
Luidenka <jarvislui1128@gmail.com>
2022-06-15 09:57:44 +0800

    Solve key in table not show

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 document.body.addEventListener('keypress', (e) => {
     document.getElementById('key').innerText = e.key;
+    document.getElementById('key-table').innerText = e.key;
     document.getElementById('keyCode').innerText = e.keyCode;
     document.getElementById('code').innerText = e.code;
     document.getElementById('location').innerText = e.location;
